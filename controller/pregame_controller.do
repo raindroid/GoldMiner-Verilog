fb21333ad089424cdc1d8428be14cee068df62ba
raindroid <yucan.wu@outlook.com>
2018-11-12 19:15:15 -0500

    Pregame_controller part done test

---
+++ controller/pregame_controller.do
@@ -0,0 +1,24 @@
+vlib work
+vlog pregame_controller.v
+vsim pregame_controller
+log {/*}
+
+add wave -color yellow clock resetn 
+add wave -color grey -hex current_state next_state 
+
+add wave -color green start mode cbk_from_reset cbk_from_view cbk_from_end_confirm
+add wave -color red user_name_req clear_req
+add wave -color blue -hex delay_counter DELAY_TIME
+
+force {clock} 0 0, 1 10ns -r 20ns
+force {resetn} 0 0, 1 20ns
+force {start} 0 0, 1 40ns, 0 60ns
+force {cbk_from_reset} 0 0, 1 90ns, 0 110ns
+
+#Test for 1 user mode
+force {mode} 0 0, 01 190ns
+force {cbk_from_view} 0 0, 0 210ns
+force {cbk_from_view} 1 490ns, 0 510ns
+force {cbk_from_view} 1 650ns, 0 670ns
+force {cbk_from_end_confirm} 0 0, 1 1010ns, 0 1030ns
+run 1300ns
--- controller/pregame_controller.v
+++ controller/pregame_controller.v
@@ -8,10 +8,10 @@
  *      clock
  *      resetn: syn low active reset
  *      start: start command at 1
- *      cbk_for_reset: callback from RESET 1 for success, 0 for in progress
- *      cbk_for_view: callback from VIEW 1 for success, 0 for in progress
- *      cbk_for_end_confirm: this is a ending-comfirmed signal given back by VIEW, 1 for success, 0 for in progress
- *      mode: this is a gloabal variable that tells us it is a 1-player(00)/2-player-co(10)/2-player-op(11) game
+ *      cbk_from_reset: callback from RESET 1 for success, 0 for in progress
+ *      cbk_from_view: callback from VIEW 1 for success, 0 for in progress
+ *      cbk_from_end_confirm: this is a ending-comfirmed signal given back by VIEW, 1 for success, 0 for in progress
+ *      mode: this is a gloabal variable that tells us it is a DNE(00)/1-player(01)/2-player-co(10)/2-player-op(11) game
                 MSB indicates whether it is 1 or 2 player
  * OUTPUT:
  *      user_name_req: 
@@ -24,8 +24,8 @@
  * Author: Yucan Wu
  * Version: 0.0.1
  * Created: Nov 12, 2018
- * Last Updated: Nov 12, 2018, just started
- * Tested: Not Yet
+ * Last Updated: Nov 12, 2018, seems work
+ * Tested: Done
  **/
 
 module test_pregame_controller(
@@ -35,13 +35,15 @@ module test_pregame_controller(
 endmodule // test_pregame_controller
 
 module pregame_controller(
-    input clock, resetn, cbk_for_view, cbk_for_end_confirm
+    input clock, resetn, 
+    input start,
+    input cbk_from_reset, cbk_from_view, cbk_from_end_confirm,
     input [1:0] mode,
 
     output reg [1:0] user_name_req,
     output reg clear_req
 );
-
+    parameter DELAY_TIME = 10'h10;
     reg [4:0] current_state, next_state;
     reg [9:0] delay_counter;
 
@@ -56,7 +58,7 @@ module pregame_controller(
                 S_REQ_2nd       = 5'd8,
                 S_WAIT_2nd      = 5'd9,
                 S_END           = 5'd10;
-    
+     
     localparam frame_clock = 833_333;
 
     //State table
@@ -64,15 +66,16 @@ module pregame_controller(
         case (current_state)
             S_START:        next_state = (start) ? S_RESET : S_START;
             S_RESET:        next_state = S_RESET_WAIT;
-            S_RESET_WAIT:   next_state = (cbk_for_reset) ? S_WAIT_MODE : S_RESET_WAIT;
-            S_WAIT_MODE:    next_state = (cbk_for_view) ? S_1st_DELAY : S_RESET_WAIT;
-            S_1st_DELAY:    next_state = (delay_counter == 100) ? S_REQ_1st : S_1st_DELAY;
+            S_RESET_WAIT:   next_state = (cbk_from_reset) ? S_WAIT_MODE : S_RESET_WAIT;
+            S_WAIT_MODE:    next_state = (mode[0] | mode[1]) ? S_1st_DELAY : S_WAIT_MODE;
+            S_1st_DELAY:    next_state = (delay_counter == DELAY_TIME) ? S_REQ_1st : S_1st_DELAY;
             S_REQ_1st:      next_state = S_WAIT_1st;
-            S_WAIT_1st:     next_state = (cbk_for_view) ? S_2nd_DELAY : S_WAIT_1st;
-            S_2nd_DELAY:    next_state = (delay_counter == 100) ? S_REQ_2st : S_2nd_DELAY;
-            S_REQ_2nd:      next_state = S_WAIT_2st;
-            S_WAIT_2nd:     next_state = (cbk_for_view) ? S_END : S_WAIT_2nd;
-            S_END:          next_state = (cbk_for_end_confirm) ? S_START : S_END;
+            S_WAIT_1st:     next_state = (cbk_from_view) ? S_2nd_DELAY : S_WAIT_1st;
+            S_2nd_DELAY:    next_state = (delay_counter == DELAY_TIME) ? 
+                                         ((mode[1]) ? S_REQ_2nd : S_END) : S_2nd_DELAY;
+            S_REQ_2nd:      next_state = S_WAIT_2nd;
+            S_WAIT_2nd:     next_state = (cbk_from_view) ? S_END : S_WAIT_2nd;
+            S_END:          next_state = (cbk_from_end_confirm) ? S_START : S_END;
           default:          next_state = S_START;
         endcase
     end
@@ -88,33 +91,43 @@ module pregame_controller(
                 clear_req = 1;
             end
             S_RESET_WAIT: begin
-                
+                //pass
             end
             S_WAIT_MODE: begin
-              
+                //prepare for delay state
+                delay_counter = 0;
             end
             S_1st_DELAY: begin
-              
+                delay_counter <= delay_counter + 1;
             end
             S_REQ_1st: begin
-              
+                user_name_req = 2'b10;
             end
             S_WAIT_1st: begin
-              
+                //prepare for delay state
+                delay_counter = 0;
             end
             S_2nd_DELAY: begin
-              
+                delay_counter <= delay_counter + 1;
             end
             S_REQ_2nd: begin
-              
+                user_name_req = 2'b11;
             end
             S_WAIT_2nd: begin
-              
+                //pass
             end
             S_END: begin
-              
+                user_name_req = 2'b01;
             end
         endcase
+    end
+
+    //state changes
+    always @(posedge clock) begin
+        if (!resetn)
+            current_state <= S_START;
+        else
+            current_state <= next_state;
     end
 
 endmodule // pregame_controller
