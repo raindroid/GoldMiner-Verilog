4ae8efbbe6b18dfbb7af495e32a01d87bb62ee0b
raindroid <36122225+yucanwu@users.noreply.github.com>
2018-11-21 16:27:54 -0500

    gfdsasdfas

--- helper/map_gen.py
+++ helper/map_gen.py
@@ -72,7 +72,7 @@ CONTENT BEGIN""");
                           "10", sep="")
                 else:
                     print(to_bin(i, 4), "\t\t:\t", to_bin(x, 13), to_bin(y, 13), to_bin(0, 4),
-                          to_bin((1 if i < gold_q else (0 if i < stone_q else 2)), 2),
+                          to_bin((1 if i < gold_q else (0 if i < stone_q + gold_q else 2)), 2),
                           "10", sep="")
                 get = True
 
